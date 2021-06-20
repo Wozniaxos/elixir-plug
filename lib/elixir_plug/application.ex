926ab5cf8df5a6fce0211404aa875eb9f1e473b4
Wozniaxos <kacperwozniax@gmail.com>
2021-06-20 17:39:41 +0200

    feat: add simple plug

---
+++ lib/elixir_plug/application.ex
@@ -0,0 +1,15 @@
+defmodule ElixirPlug.Application do
+  use Application
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: ElixirPlug.Router, options: [port: 8080]}
+    ]
+    opts = [strategy: :one_for_one, name: ElixirPlug.Supervisor]
+
+    Logger.info("Starting application...")
+
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ lib/elixir_plug/router.ex
@@ -0,0 +1,15 @@
+defmodule ElixirPlug.Router do
+  use Plug.Router
+
+  plug Plug.Logger
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Welcome")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+end
